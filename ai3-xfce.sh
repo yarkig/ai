3ebbbb597cb86406612297401df9127b2a66990f
yarkig <48361111+yarkig@users.noreply.github.com>
2019-11-18 20:00:46 +0300

    Update ai3-xfce.sh

--- ai3-xfce.sh
+++ ai3-xfce.sh
@@ -1,20 +1,20 @@
-echo 'Прогресс-бар в виде Пакмана, пожирающего пилюли'
+echo '\033[32mПрогресс-бар в виде Пакмана, пожирающего пилюли'
 sudo sed -ie '/^# Misc options/a ILoveCandy' /etc/pacman.conf
 
-echo 'Ставим DE, драйвера и основные программы'
+echo '\033[32mСтавим DE, драйвера и основные программы'
 sudo pacman -S gvfs network-manager-applet xf86-video-intel xorg xfce4 xfce4-goodies screenfetch pavucontrol pulseaudio vlc ttf-liberation ttf-dejavu zsh telegram-desktop chromium xdg-user-dirs --noconfirm
 
-echo 'Ставим AUR (yay) и pamac-aur'
+echo '\033[32mСтавим AUR (yay) и pamac-aur'
 sudo pacman -Syu --noconfirm
 (
   echo;
 ) | sh -c "$(curl -fsSL git.io/yay-install.sh)" --noconfirm
 yay -S pamac-aur --noconfirm
 
-echo 'Ставим темы и иконки'
+echo '\033[32mСтавим темы и иконки'
 yay -S numix-frost-themes numix-circle-icon-theme-git --noconfirm
 
-echo 'Ставим oh-my-zsh'
+echo '\033[32mСтавим oh-my-zsh'
 yay -S oh-my-zsh-git --noconfirm
 sudo sed -ie 's/robbyrussell/bureau/' /usr/share/oh-my-zsh/zshrc
 cp /usr/share/oh-my-zsh/zshrc /home/igor/.zshrc
@@ -22,7 +22,7 @@ sudo cp /usr/share/oh-my-zsh/zshrc /root/.zshrc
 sudo chsh -s /bin/zsh igor
 sudo chsh -s /bin/zsh root
 
-echo 'Автозапуск XFCE4'
+echo '\033[32mАвтозапуск XFCE4'
 sudo mkdir /etc/systemd/system/getty@tty1.service.d
 echo "[Service]" | sudo tee --append /etc/systemd/system/getty@tty1.service.d/override.conf > /dev/null
 echo "ExecStart=" | sudo tee --append /etc/systemd/system/getty@tty1.service.d/override.conf > /dev/null
@@ -31,10 +31,10 @@ echo '[[ -f ~/.zshrc ]] && . ~/.zshrc' > /home/igor/.zprofile
 echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> /home/igor/.zprofile
 echo "exec startxfce4" > /home/igor/.xinitrc
 
-echo 'Создаем пользовательские директории'
+echo '\033[32mСоздаем пользовательские директории'
 xdg-user-dirs-update
 
-echo 'Устанавливаем конфиг XFCE4'
+echo '\033[32mУстанавливаем конфиг XFCE4'
 wget git.io/ai-conf.tar.gz
 wget git.io/ai-wp.tar.gz
 sudo rm -rf ~/.config
