eeaf8597de2aee572ef3befbb676ecb3f50c07b9
yarkig <48361111+yarkig@users.noreply.github.com>
2019-11-18 19:58:20 +0300

    Update ai2-uefi.sh

--- ai2-uefi.sh
+++ ai2-uefi.sh
@@ -1,27 +1,27 @@
 read -p "Введите имя компьютера: " hostname
-echo 'Прписываем имя компьютера'
+echo '\033[32mПрписываем имя компьютера'
 echo $hostname > /etc/hostname
 ln -svf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 
-echo 'Добавляем русскую локаль системы'
+echo '\033[32mДобавляем русскую локаль системы'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
 
-echo 'Обновим текущую локаль системы'
+echo '\033[32mОбновим текущую локаль системы'
 locale-gen
 
-echo 'Указываем язык системы'
+echo '\033[32mУказываем язык системы'
 echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
 export LANG=ru_RU.UTF-8
 
-echo 'Консольный шрифт и раскладка клавиатуры'
+echo '\033[32mКонсольный шрифт и раскладка клавиатуры'
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 
-echo 'Создадим загрузочный RAM диск'
+echo '\033[32mСоздадим загрузочный RAM диск'
 mkinitcpio -p linux
 
-echo 'Устанавливаем загрузчик'
+echo '\033[32mУстанавливаем загрузчик'
 bootctl --path=/boot install
 echo "default arch" > /boot/loader/loader.conf
 echo "title Arch Linux" > /boot/loader/entries/arch.conf
@@ -30,20 +30,20 @@ echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
 echo "options root=/dev/sda3 rw" >> /boot/loader/entries/arch.conf
 
-echo 'Устанавливаем пароль root'
+echo '\033[32mУстанавливаем пароль root'
 passwd
 
-echo 'Добавляем пользователя'
+echo '\033[32mДобавляем пользователя'
 useradd -m -g users -G wheel -s /bin/bash igor
 
-echo 'Устанавливаем пароль пользователя'
+echo '\033[32mУстанавливаем пароль пользователя'
 passwd igor
 
-echo 'Устанавливаем SUDO'
+echo '\033[32mУстанавливаем SUDO'
 echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
 
-echo 'Включим нужные службы:'
+echo '\033[32mВключим нужные службы'
 systemctl enable sshd NetworkManager
 
-echo 'Выходим из окружения chrootexit'
+echo '\033[32mВыходим из окружения chrootexit'
 exit
