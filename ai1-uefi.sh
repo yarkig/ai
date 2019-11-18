83754509b4951c0415f507bd65d542eb20e6c52f
yarkig <48361111+yarkig@users.noreply.github.com>
2019-11-18 20:21:40 +0300

    Update ai1-uefi.sh

--- ai1-uefi.sh
+++ ai1-uefi.sh
@@ -1,10 +1,10 @@
 loadkeys ru
 setfont cyr-sun16
 
-echo '\033[32mОчистка диска'
+echo 'Очистка диска'
 dd if=/dev/zero of=/dev/sda bs=32M status=progress
 
-echo '\033[32mСоздание разделов'
+echo 'Создание разделов'
 (
   echo g;
 
@@ -31,28 +31,28 @@ echo '\033[32mСоздание разделов'
   echo w;
 ) | fdisk /dev/sda
 
-echo '\033[32mФорматирование дисков'
+echo 'Форматирование дисков'
 mkfs.vfat -F32 /dev/sda1
 mkswap /dev/sda2 -L swap
 mkfs.ext4  /dev/sda3
 
-echo '\033[32mМонтирование дисков'
+echo 'Монтирование дисков'
 mount /dev/sda3 /mnt
 mkdir /mnt/{boot,home}
 mount /dev/sda1 /mnt/boot
 swapon /dev/sda2
 
-echo '\033[32mВыбор зеркал для загрузки. Ставим зеркало от Яндекс на первое место'
+echo 'Выбор зеркал для загрузки. Ставим зеркало от Яндекс на первое место'
 sed -ie '1 iServer = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch' /etc/pacman.d/mirrorlist
 
-echo '\033[32mПрогресс-бар в виде Пакмана, пожирающего пилюли'
+echo 'Прогресс-бар в виде Пакмана, пожирающего пилюли'
 sudo sed -ie '/^# Misc options/a ILoveCandy' /etc/pacman.conf
 
-echo '\033[32mУстановка основных пакетов'
+echo 'Установка основных пакетов'
 pacstrap -i /mnt base base-devel linux linux-firmware mc nano openssh networkmanager intel-ucode --noconfirm
 
-echo '\033[32mНастройка системы'
+echo 'Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-echo '\033[32mВходим в установленную систему'
+echo 'Входим в установленную систему'
 arch-chroot /mnt sh -c "$(curl -fsSL git.io/ai2-uefi.sh)"
